952e9890df1b85dd016214a711885b1ce2516202
KemingChen <believe75467@gmail.com>
2014-02-11 17:08:31 +0800

    add updateUI & initialUI

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var criterions = {"1001001":{"category":"△","subjectId":"1001001","subjectName":"體育","remark":"　"},"1004001":{"category":"☆","subjectId":"1004001","subjectName":"體育","remark":"　"},"1004002":{"category":"☆","subjectId":"1004002","subjectName":"體育","remark":"　"},"1101001":{"category":"△","subjectId":"1101001","subjectName":"軍訓(一)","remark":"　"},"1101002":{"category":"△","subjectId":"1101002","subjectName":"軍訓(二)","remark":"　"},"1102004":{"category":"☆","subjectId":"1102004","subjectName":"軍訓(三)","remark":"　"},"1102005":{"category":"☆","subjectId":"1102005","subjectName":"軍訓(四)","remark":"　"},"1400026":{"category":"△","subjectId":"1400026","subjectName":"英文實務","remark":"　"},"1400027":{"category":"△","subjectId":"1400027","subjectName":"英文閱讀與聽講練習","remark":"　"},"1400028":{"category":"△","subjectId":"1400028","subjectName":"進階英文閱讀與聽講練習","remark":"　"},"1400098":{"category":"△","subjectId":"1400098","subjectName":"大學入門","remark":"　"},"1400099":{"category":"△","subjectId":"1400099","subjectName":"服務學習","remark":"　"},"1401032":{"category":"▲","subjectId":"1401032","subjectName":"微積分","remark":"＊"},"1401041":{"category":"▲","subjectId":"1401041","subjectName":"物理","remark":"＊"},"1401043":{"category":"▲","subjectId":"1401043","subjectName":"物理實驗","remark":"＊"},"1404003":{"category":"△","subjectId":"1404003","subjectName":"國文","remark":"　"},"1419981":{"category":"△","subjectId":"1419981","subjectName":"博雅核心課程-文學","remark":"　"},"1419982":{"category":"△","subjectId":"1419982","subjectName":"博雅核心課程-歷史","remark":"　"},"1419983":{"category":"△","subjectId":"1419983","subjectName":"博雅核心課程-哲學","remark":"　"},"1419984":{"category":"△","subjectId":"1419984","subjectName":"博雅核心課程-法治","remark":"　"},"1419985":{"category":"△","subjectId":"1419985","subjectName":"博雅核心課程-社會","remark":"　"},"1419991":{"category":"△","subjectId":"1419991","subjectName":"博雅選修課程","remark":"　"},"1419992":{"category":"△","subjectId":"1419992","subjectName":"博雅選修課程","remark":"　"},"1419993":{"category":"△","subjectId":"1419993","subjectName":"博雅選修課程","remark":"　"},"1419994":{"category":"△","subjectId":"1419994","subjectName":"博雅選修課程","remark":"　"},"1419995":{"category":"△","subjectId":"1419995","subjectName":"博雅選修課程","remark":"　"},"3102081":{"category":"★","subjectId":"3102081","subjectName":"電子學(一)","remark":"◎"},"3102084":{"category":"★","subjectId":"3102084","subjectName":"電子學(二)","remark":"◎"},"3102091":{"category":"★","subjectId":"3102091","subjectName":"電子學實習(一)","remark":"◎"},"3102092":{"category":"★","subjectId":"3102092","subjectName":"電子學實習(二)","remark":"◎"},"3102097":{"category":"★","subjectId":"3102097","subjectName":"機率","remark":"◎"},"3103064":{"category":"★","subjectId":"3103064","subjectName":"訊號與系統","remark":"◎"},"3201009":{"category":"★","subjectId":"3201009","subjectName":"化學","remark":"◎"},"3601005":{"category":"★","subjectId":"3601005","subjectName":"數位邏輯設計","remark":"◎"},"3601011":{"category":"▲","subjectId":"3601011","subjectName":"計算機程式語言","remark":"＊"},"3601012":{"category":"▲","subjectId":"3601012","subjectName":"電機資訊科技","remark":"＊"},"3602051":{"category":"★","subjectId":"3602051","subjectName":"計算機演算法","remark":"◎"},"3603051":{"category":"★","subjectId":"3603051","subjectName":"離散數學","remark":"◎"},"3603053":{"category":"★","subjectId":"3603053","subjectName":"複變函數","remark":"◎"},"3603054":{"category":"▲","subjectId":"3603054","subjectName":"線性代數","remark":"＊"},"3603059":{"category":"★","subjectId":"3603059","subjectName":"作業系統","remark":"◎"},"3603082":{"category":"★","subjectId":"3603082","subjectName":"計算機組織","remark":"◎"},"5901202":{"category":"▲","subjectId":"5901202","subjectName":"計算機概論","remark":"＊"},"5901206":{"category":"★","subjectId":"5901206","subjectName":"物件導向程式設計","remark":"◎"},"5901211":{"category":"★","subjectId":"5901211","subjectName":"數位邏輯設計實習","remark":"　"},"5902201":{"category":"★","subjectId":"5902201","subjectName":"資料結構","remark":"◎"},"5902205":{"category":"★","subjectId":"5902205","subjectName":"網際網路技術與應用","remark":"　"},"5902206":{"category":"▲","subjectId":"5902206","subjectName":"資料庫系統","remark":"■"},"5902207":{"category":"★","subjectId":"5902207","subjectName":"微算機系統","remark":"◎"},"5902209":{"category":"▲","subjectId":"5902209","subjectName":"物件導向程式設計實習","remark":"■"},"5902301":{"category":"★","subjectId":"5902301","subjectName":"數據通訊系統概論","remark":"　"},"5902302":{"category":"★","subjectId":"5902302","subjectName":"資訊管理概論","remark":"　"},"5902306":{"category":"★","subjectId":"5902306","subjectName":"多媒體技術與應用","remark":"　"},"5903202":{"category":"★","subjectId":"5903202","subjectName":"系統程式","remark":"　"},"5903203":{"category":"▲","subjectId":"5903203","subjectName":"計算機網路","remark":"■"},"5903204":{"category":"▲","subjectId":"5903204","subjectName":"實務專題(一)","remark":"■"},"5903205":{"category":"★","subjectId":"5903205","subjectName":"自動機理論","remark":"　"},"5903208":{"category":"▲","subjectId":"5903208","subjectName":"實務專題(二)","remark":"■"},"5903209":{"category":"★","subjectId":"5903209","subjectName":"微算機系統實習","remark":"　"},"5903301":{"category":"★","subjectId":"5903301","subjectName":"視窗程式設計","remark":"　"},"5903302":{"category":"★","subjectId":"5903302","subjectName":"計算機圖學","remark":"　"},"5903303":{"category":"★","subjectId":"5903303","subjectName":"JAVA技術與應用","remark":"　"},"5903304":{"category":"★","subjectId":"5903304","subjectName":"計算機結構","remark":"　"},"5903305":{"category":"★","subjectId":"5903305","subjectName":"數位系統設計","remark":"　"},"5903306":{"category":"★","subjectId":"5903306","subjectName":"電子電路(二)","remark":"　"},"5903307":{"category":"★","subjectId":"5903307","subjectName":"電子電路實習(二)","remark":"　"},"5903308":{"category":"★","subjectId":"5903308","subjectName":"科技管理","remark":"　"},"5903309":{"category":"★","subjectId":"5903309","subjectName":"科技英文","remark":"　"},"5903311":{"category":"★","subjectId":"5903311","subjectName":"程式語言原理","remark":"　"},"5903312":{"category":"★","subjectId":"5903312","subjectName":"驅動程式設計","remark":"　"},"5903313":{"category":"★","subjectId":"5903313","subjectName":"數值方法","remark":"　"},"5903314":{"category":"★","subjectId":"5903314","subjectName":"數位信號處理","remark":"　"},"5903315":{"category":"★","subjectId":"5903315","subjectName":"數位通訊","remark":"　"},"5903317":{"category":"★","subjectId":"5903317","subjectName":"超大型積體電路導論","remark":"　"},"5904202":{"category":"▲","subjectId":"5904202","subjectName":"實務專題(三)","remark":"■"},"5904301":{"category":"★","subjectId":"5904301","subjectName":"網路程式設計","remark":"　"},"5904302":{"category":"★","subjectId":"5904302","subjectName":"軟體工程","remark":"　"},"5904303":{"category":"★","subjectId":"5904303","subjectName":"軟體技術認證","remark":"　"},"5904304":{"category":"★","subjectId":"5904304","subjectName":"電子商務技術","remark":"　"},"5904305":{"category":"★","subjectId":"5904305","subjectName":"編譯器原理","remark":"　"},"5904307":{"category":"★","subjectId":"5904307","subjectName":"數位信號處理實習","remark":"　"},"5904308":{"category":"★","subjectId":"5904308","subjectName":"無線與行動通訊系統","remark":"　"},"5904309":{"category":"★","subjectId":"5904309","subjectName":"電腦週邊介面","remark":"　"},"5904310":{"category":"★","subjectId":"5904310","subjectName":"資訊安全","remark":"　"},"5904312":{"category":"★","subjectId":"5904312","subjectName":"人機互動","remark":"　"},"5904313":{"category":"★","subjectId":"5904313","subjectName":"數位影像處理","remark":"　"},"5904314":{"category":"★","subjectId":"5904314","subjectName":"MPEG技術","remark":"　"},"5904315":{"category":"★","subjectId":"5904315","subjectName":"人工智慧","remark":"　"},"5904316":{"category":"★","subjectId":"5904316","subjectName":"圖形理論","remark":"　"},"5904317":{"category":"★","subjectId":"5904317","subjectName":"類神經網路","remark":"　"},"5904319":{"category":"★","subjectId":"5904319","subjectName":"嵌入式系統","remark":"　"},"5904320":{"category":"★","subjectId":"5904320","subjectName":"生物資訊概論","remark":"　"},"5904322":{"category":"★","subjectId":"5904322","subjectName":"設計樣式","remark":"　"},"5904325":{"category":"★","subjectId":"5904325","subjectName":"行動網際網路技術與應用","remark":"　"},"5904326":{"category":"★","subjectId":"5904326","subjectName":"視訊信號處理","remark":"　"},"5904327":{"category":"★","subjectId":"5904327","subjectName":"隨機信號與系統","remark":"　"},"5904335":{"category":"★","subjectId":"5904335","subjectName":"平行程式設計","remark":"　"},"5904336":{"category":"★","subjectId":"5904336","subjectName":"新產品企劃","remark":"　"},"6502004":{"category":"▲","subjectId":"6502004","subjectName":"電路學","remark":"＊"},"6502006":{"category":"★","subjectId":"6502006","subjectName":"幾何光學","remark":"◎"},"6503303":{"category":"★","subjectId":"6503303","subjectName":"電磁學(一)","remark":"◎"},"6503304":{"category":"★","subjectId":"6503304","subjectName":"電磁學(二)","remark":"◎"},"6503309":{"category":"★","subjectId":"6503309","subjectName":"光電工程概論","remark":"◎"},"6503507":{"category":"▲","subjectId":"6503507","subjectName":"微分方程","remark":"＊"}};
 var rCriterions = {"體育":"1004002","軍訓(一)":"1101001","英文閱讀與聽講練習":"1400027","大學入門":"1400098","國文":"1404003","博雅核心課程-社會":"1419985","微積分":"1401032","物理":"1401041","物理實驗":"1401043","計算機程式語言":"3601011","電機資訊科技":"3601012","計算機概論":"5901202","數位邏輯設計":"3601005","軍訓(二)":"1101002","服務學習":"1400099","博雅核心課程-法治":"1419984","線性代數":"3603054","化學":"3201009","物件導向程式設計":"5901206","微算機系統":"5902207","進階英文閱讀與聽講練習":"1400028","博雅核心課程-文學":"1419981","博雅核心課程-歷史":"1419982","電路學":"6502004","微分方程":"6503507","軍訓(三)":"1102004","電子學(一)":"3102081","電子學實習(一)":"3102091","訊號與系統":"3103064","離散數學":"3603051","資料結構":"5902201","數據通訊系統概論":"5902301","資訊管理概論":"5902302","幾何光學":"6502006","電磁學(一)":"6503303","博雅核心課程-哲學":"1419983","博雅選修課程":"1419995","軍訓(四)":"1102005","電子學(二)":"3102084","電子學實習(二)":"3102092","機率":"3102097","計算機演算法":"3602051","複變函數":"3603053","計算機組織":"3603082","數位邏輯設計實習":"5901211","網際網路技術與應用":"5902205","多媒體技術與應用":"5902306","電磁學(二)":"6503304","光電工程概論":"6503309","計算機網路":"5903203","實務專題(一)":"5903204","系統程式":"5903202","微算機系統實習":"5903209","計算機圖學":"5903302","JAVA技術與應用":"5903303","計算機結構":"5903304","數位系統設計":"5903305","電子電路(二)":"5903306","電子電路實習(二)":"5903307","科技管理":"5903308","設計樣式":"5904322","英文實務":"1400026","資料庫系統":"5902206","物件導向程式設計實習":"5902209","實務專題(二)":"5903208","作業系統":"3603059","自動機理論":"5903205","視窗程式設計":"5903301","科技英文":"5903309","程式語言原理":"5903311","驅動程式設計":"5903312","數值方法":"5903313","數位信號處理":"5903314","數位通訊":"5903315","超大型積體電路導論":"5903317","實務專題(三)":"5904202","網路程式設計":"5904301","軟體工程":"5904302","軟體技術認證":"5904303","電子商務技術":"5904304","編譯器原理":"5904305","數位信號處理實習":"5904307","無線與行動通訊系統":"5904308","電腦週邊介面":"5904309","資訊安全":"5904310","行動網際網路技術與應用":"5904325","視訊信號處理":"5904326","隨機信號與系統":"5904327","新產品企劃":"5904336","人機互動":"5904312","數位影像處理":"5904313","MPEG技術":"5904314","人工智慧":"5904315","圖形理論":"5904316","類神經網路":"5904317","嵌入式系統":"5904319","生物資訊概論":"5904320","平行程式設計":"5904335"};
-var stipulate = "Test Stipulate ... bra bra bra";
+var stipulate = "1.最低畢業學分：139學分。<br />\
+2.校訂共同必修：34學分；校訂＊基礎必修：31學分；◎核心選修課程：須修滿29學分且必須包含：數位邏輯設計、離散數學、作業系統、機率、資料結構、計算機組織、物件導向程式設計、微算機系統，多修之學分得採計為專業選修學分；主專業必修及選修：45學分須包含：■必修課程14學分及資訊工程系開設或承認之選修科目22學分。<br />\
+3.大三、大四本班學生依其選定之主修系，至各系修習專業課程可不受年級之限制。<br />\
+4.學生畢業需符合大學部英文畢業門檻，相關畢業標準請至教務處網站查詢。<br />\
+5.修習「英文實務」課程需參加全民英檢中級初試，其中「聽力測驗」及「閱讀測驗」兩項成績合計達80分以上，始得修習。<br />\
+6.跨系所專業選修，最多承認畢業學分為9學分。<br />\
+7.選讀博雅(核心)課程向度：1.文學與藝術、2.歷史思維與世界文明、3.哲學思考與倫理、4.民主與法治、5.社會經濟與管理。博雅選修課程必選科目：哲學思考與倫理向度-工程倫理。<br />\
+8.本課程科目表適用99學年度入學新生。";
 
 var test;
 
 Ntut = (function(){
+	var initial = false;
 	var symbol = {"○": "部訂共同必修", "△": "校訂共同必修", "☆": "共同選修", "●": "部訂專業必修", "▲": "校訂專業必修", "★": "專業選修"};
+	var calculateResult;
 
 	function calculate(){
 		var subjects;
@@ -16,11 +25,7 @@ Ntut = (function(){
 		subjects = getSubjects(semesters);
 		items = normalAnalysis(subjects, {"○": 0, "△": 0, "☆": 0, "●": 0, "▲": 0, "★": 0});
 
-		console.log(symbol);
-		console.log(items);
-		console.log(advanceAnalysis(subjects, {"＊": 0, "◎": 0, "■": 0}));
-		console.log(stipulate);
-		test = subjects;
+		updateUI(symbol, items, subjects, stipulate);
 	}
 
 	function normalAnalysis(subjects, items){
@@ -31,9 +36,9 @@ Ntut = (function(){
 		return items;
 	}
 
-	function advanceAnalysis(subjects, items){// Mark 分析
+	function markAnalysis(subjects, items){// Mark 分析
 		for(var i in subjects){
-			if(items[subjects[i].remark] != undefined){// 若有此 Mark 則計算
+			if(items[subjects[i].remark] != undefined){// 若有此 mark 則計算
 				items[subjects[i].remark] += subjects[i].credit;
 			}
 		}
@@ -100,16 +105,8 @@ Ntut = (function(){
 		else if(!criterions[result.subjectId]){// 若查不到則記錄LOG
 			if(!rCriterions[result.subjectName]){// 要去對應"課程名稱"可能可以抵免
 				console.log("Can't Find: " + result.subjectId + "-" + result.subjectName);
-
-				if(result.category == "暑"){// 計算暑期實習
-					criterion = {category: "★", remark: "　"};
-				}
-				else if(result.category == "選"){// 計算其他選修課程
-					criterion = {category: "★", remark: "　"};
-				}
-				else{
-					console.log("error");
-				}
+				criterion = {category: "Ｘ", remark: "　"};// 無法抵免
+				console.log(result.subjectName + " 無法抵免!");
 			}
 			else{
 				criterion = criterions[rCriterions[result.subjectName]];
@@ -144,9 +141,42 @@ Ntut = (function(){
 		});*/
 	}
 
+	function updateUI(symbol, items, subjects, stipulate){
+		console.log(symbol);
+		console.log(items);
+		console.log(markAnalysis(subjects, {"＊": 0, "◎": 0, "■": 0}));
+		console.log(stipulate);
+
+		var CRBody = calculateResult.children("tbody");
+		CRBody.empty();
+		for(var i in symbol){
+			CRBody.append("<tr><td bgcolor='#99FF99'>　(" + i + ") " + symbol[i] + "　</td><td>　" + items[i] + "　</td></tr>");
+		}
+		calculateResult.show();
+		test = calculateResult;
+	}
+
+	function initialUI(){
+		if(!initial){
+			form = $("form");
+			form.first().append('　<input type="button" name="CalculateAll" value="計算學分" onclick="Ntut.calculate()">');
+
+			calculateResult = $('<table/>', {
+			    id: 'CalculateResult', 
+			    border: 1,
+			    bgcolor: "#CCFFFF"
+			}).hide()
+			.append($("<thead/>"))
+			.append($("<tbody/>"));
+			form.after(calculateResult);
+		}
+		initial = true;
+	}
+
 	return{
+		init:　initialUI,
 		calculate: calculate
 	}
 })();
 
-Ntut.calculate();
+Ntut.init();
